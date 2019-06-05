2359cc556303e38fbc3cdd7347791ba19a2824ce
Bob Bauer <bauerbo@bauerbo-a01.vmware.com>
2019-06-05 12:14:51 -0700

    adjusted escape

--- ncp_info.sh
+++ ncp_info.sh
@@ -7,5 +7,5 @@ cat /var/log/ncp/10-nsx.conf >"${RESULTS_DIR}/ncp_info"
 # echo -n "${RESULTS_DIR}/ncp_info" >"${RESULTS_DIR}/done"
 curl -k -u admin:D1llh0le! https://10.173.62.62/api/v1/upgrade/nodes-summary >"${RESULTS_DIR}/api_info"
 # echo -n "${RESULTS_DIR}/api_info" >"${RESULTS_DIR}/done"
-cat "${RESULTS_DIR}/ncp_info ${RESULTS_DIR}/api_info" >"${$RESULTS_DIR}/done"
+cat "'${RESULTS_DIR}/ncp_info' '${RESULTS_DIR}/api_info'" >"${$RESULTS_DIR}/done"
 
