da439bbd78f9fa5ac90239c5fabc9253c7a88c7c
Bob Bauer <bauerbo@bauerbo-a01.vmware.com>
2019-06-05 11:23:34 -0700

    added curl command

--- ncp_info.sh
+++ ncp_info.sh
@@ -5,5 +5,7 @@ RESULTS_DIR="${RESULTS_DIR:-/tmp/results}"
 
 cat /var/log/ncp/10-nsx.conf >"${RESULTS_DIR}/ncp_info"
 echo -n "${RESULTS_DIR}/ncp_info" >"${RESULTS_DIR}/done"
+curl -u admin:D1llh0le! https://10.173.62.62/api/v1/upgrade/nodes-summary >"${RESULTS_DIR}/api_info"
+echo -n "${RESULTS_DIR}/api_info" >"${RESULTS_DIR}/done"
 
 
