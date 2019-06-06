b8f0f8069f5026d4806f6e62b50a8361f0bb1519
rotbau <37818713+rotbau@users.noreply.github.com>
2019-06-05 17:32:00 -0700

    Update api_info.sh

--- api_info.sh
+++ api_info.sh
@@ -5,6 +5,6 @@ RESULTS_DIR="${RESULTS_DIR:-/tmp/results}"
 
 
 # Get node summary from NSX Manager API
-curl -k -u admin:D1llh0le! https://10.173.62.62/api/v1/upgrade/nodes-summary >"${RESULTS_DIR}/api_info"
+curl -k -u admin:VMware1! https://10.173.62.62/api/v1/upgrade/nodes-summary >"${RESULTS_DIR}/api_info"
 echo -n "${RESULTS_DIR}/api_info" >"${RESULTS_DIR}/done"
 
